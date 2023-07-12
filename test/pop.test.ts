27523167cde91ded8863741b87fc91787b4903a0
gimpey <gimpey@pr0xy.io>
2023-07-12 13:14:20 -0400

    resolving tests for interaction with bayc contract

--- test/pop.test.ts
+++ test/pop.test.ts
@@ -1,8 +1,8 @@
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 import { ZERO_ADDRESS } from "./utils/constants";
-import { parseEther } from "ethers";
+import { parseEther, formatEther } from "ethers";
 import { expect } from "chai";
-import hre from "hardhat";
+import hre, { ethers } from "hardhat";
 
 import "@nomicfoundation/hardhat-chai-matchers";
 import "@nomicfoundation/hardhat-ethers";
@@ -10,7 +10,7 @@ import "@nomicfoundation/hardhat-ethers";
 const config = {
   startingPrice: parseEther("0.04"),
   updatedPrice: parseEther("0.02"),
-  generateTest: [53, 107, 95],
+  generateTest: [6, 9, 1],
 };
 
 /**
@@ -18,9 +18,12 @@ const config = {
  * @description Test suite for Proof of Purchase token.
  *
  * @resource https://hardhat.org/tutorial
+ *
+ * todo: beforeAll instead of beforeEach (https://stackoverflow.com/questions/37912397)
  */
 describe("Proof of Purchase", () => {
   let pop: any;
+  let bayc: any;
   let owner: HardhatEthersSigner;
   let user: HardhatEthersSigner;
   let receiver: HardhatEthersSigner;
@@ -35,12 +38,33 @@ describe("Proof of Purchase", () => {
     receiver = signers[2];
     vault = signers[3];
 
-    const primary = ZERO_ADDRESS;
-    pop = await hre.ethers.deployContract("TokenGated", [
-      vault.address,
-      primary,
-      config.startingPrice,
+    // deploys a bored ape contract to test interactions with it
+    bayc = await hre.ethers.deployContract("BoredApeYachtClub", [
+      "Bored Ape Yacht Club",
+      "BAYC",
+      10000,
+      1619060596,
     ]);
+
+    pop = await hre.ethers.deployContract("TokenGated", [
+      bayc.target,
+      config.startingPrice,
+      [vault.address],
+      [100],
+    ]);
+
+    // ensuring the sale is active for testing
+    await bayc.connect(owner).flipSaleState();
+    const state = await bayc.saleIsActive();
+    expect(state).to.equal(true);
+  });
+
+  /** @description Running some basic BAYC functions. */
+  describe("Bored Ape Yacht Club", () => {
+    it("allows someone to purchase a BAYC", async () => {
+      await bayc.connect(user).mintApe(5, { value: parseEther("0.40") });
+      expect(await bayc.balanceOf(user.address)).to.equal(5);
+    });
   });
 
   /** @description Tests related to initial deployment. */
@@ -50,10 +74,11 @@ describe("Proof of Purchase", () => {
       expect(contractOwner).to.equal(owner.address);
     });
 
-    it("properly sets the vault", async () => {
-      const contractVault = await pop.vault();
-      expect(contractVault).to.equal(vault.address);
-    });
+    // todo: implement this for the payment splitter
+    // it("properly sets the vault", async () => {
+    //   const contractVault = await pop.vault();
+    //   expect(contractVault).to.equal(vault.address);
+    // });
 
     it("properly initializes the price", async () => {
       const contractPrice = await pop.price();
@@ -92,12 +117,16 @@ describe("Proof of Purchase", () => {
       ).to.be.revertedWith("purchasing receipts is not active");
     });
 
-    it("allows the owner to generate a tokens for a user", async () => {
+    it("allows the owner to generate tokens for a user", async () => {
+      // allocating 10 board apes to the users wallet
+      await bayc.connect(user).mintApe(10, { value: parseEther("0.80") });
+      expect(await bayc.balanceOf(user.address)).to.equal(10);
+
       await pop.connect(owner).setActive(true);
 
-      await pop.connect(owner).generate(receiver.address, config.generateTest);
+      await pop.connect(owner).generate(user.address, config.generateTest);
 
-      expect(await pop.balanceOf(receiver.address)).to.equal(3);
+      expect(await pop.balanceOf(user.address)).to.equal(3);
 
       config.generateTest.forEach(async (primaryToken, index) => {
         expect(await pop.ownerOf(index)).to.equal(receiver.address);
@@ -109,18 +138,33 @@ describe("Proof of Purchase", () => {
   /** @description Ensuring the tokens are soulbound (untradable). */
   describe("Soulbound", () => {
     it("restricts transfers after mint", async () => {
+      // allocating 1 board ape to the users wallet
+      await bayc.connect(user).mintApe(1, { value: parseEther("0.08") });
+      expect(await bayc.balanceOf(user.address)).to.equal(1);
+
       await pop.connect(owner).setActive(true);
 
-      await pop.connect(owner).generate(receiver.address, [1]);
+      await pop.connect(owner).generate(user.address, [0]);
+
+      expect(await pop.balanceOf(user.address)).to.equal(1);
 
       await expect(
-        pop.connect(receiver).transferFrom(receiver.address, user.address, 1)
+        pop.connect(user).transferFrom(user.address, receiver.address, 1)
+      ).to.be.revertedWith("token is soulbound");
+
+      await expect(
+        pop.connect(user).safeTransferFrom(user.address, receiver.address, 1)
       ).to.be.revertedWith("token is soulbound");
 
       await expect(
         pop
-          .connect(receiver)
-          .safeTransferFrom(receiver.address, user.address, 1)
+          .connect(user)
+          ["safeTransferFrom(address,address,uint256,bytes)"](
+            user.address,
+            receiver.address,
+            1,
+            "0x01"
+          )
       ).to.be.revertedWith("token is soulbound");
     });
   });
