455e195794113c005e3d2da33fb1a5d1ca50802b
gimpey <gimpey@pr0xy.io>
2023-07-13 13:14:15 -0400

    implementing the ability to deploy contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,42 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import getEnv from "./utils/getEnv";
+import dotenv from "dotenv";
 import "solidity-coverage";
+
+dotenv.config({
+  path: `${__dirname}/${getEnv(process.env.NODE_ENV)}`,
+});
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  defaultNetwork: "hardhat",
+  mocha: {
+    timeout: 10_000,
+  },
 };
+
+if (process.env.NODE_ENV === "deploy") {
+  config.defaultNetwork = "hardhat";
+  config.etherscan = {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  };
+  config.networks = {
+    hardhat: {},
+    mainnet: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
+    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
+    },
+  };
+}
 
 /** @type import('hardhat/config').HardhatUserConfig */
 export default config;
---
+++ scripts/deployment/deployBayc.ts
@@ -0,0 +1,70 @@
+import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-verify";
+import hre, { ethers } from "hardhat";
+import fs from "fs";
+
+/**
+ * @title Deploy
+ * @description Deploys the related contract to the desired network. The script
+ * then saves the deployment information to a json file for later reference or
+ * use. Following successful deployment, the script attempts to verify the
+ * contract on Etherscan.
+ *
+ * @resource https://docs.alchemy.com/docs/hello-world-smart-contract
+ * @resource https://hardhat.org/hardhat-runner/plugins/nomicfoundation-hardhat-verify
+ */
+const deploy = async () => {
+  const [deployer] = await hre.ethers.getSigners();
+  const network = hre.network.name;
+
+  console.log(`Deploying contracts with the account: ${deployer.address}`);
+  console.log(`Deploying Bored Ape Yacht Club [Test] to \`${network}\`.`);
+
+  // defining the arguments for bored apes
+  const boredApeArgs = ["Bored Ape Yacht Club", "BAYC", 10000, 1619060596];
+
+  // deploy the bored ape yacht club contract
+  const bayc = await ethers.deployContract("BoredApeYachtClub", boredApeArgs);
+  console.log(`Transaction pending with target address ${bayc.target}.`);
+  await bayc.waitForDeployment();
+
+  // Start deployment, returning a promise that resolves to a contract object
+  console.log(`Contract successfully deployed to ${bayc.target}.`);
+  const timestamp = Date.now();
+
+  // exporting the information as a json file for use with the POP contract
+  const deployment = JSON.stringify(
+    {
+      deployer: deployer.address,
+      chain: network,
+      contractAddress: bayc.target,
+      arguments: boredApeArgs,
+      timestamp,
+    },
+    null,
+    2
+  );
+
+  // writing the data to a file to be referenced by the POP contract
+  fs.writeFileSync(
+    `./deployments/deploymentBayc_${timestamp}.json`,
+    deployment,
+    "utf-8"
+  );
+
+  console.log(`Waiting a minute before verifying the contract...`);
+  await new Promise((resolve) => setTimeout(resolve, 60_000));
+
+  // running the verification step
+  console.log(`Attempting to verify the contract at ${bayc.target}.`);
+  try {
+    await hre.run("verify:verify", {
+      address: bayc.target,
+      constructorArguments: boredApeArgs,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+deploy();
