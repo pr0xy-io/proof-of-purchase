38120219ec43defa1891acfeaaf35778707d3b83
gimpey <gimpey@pr0xy.io>
2023-08-01 23:51:42 -0400

    testing withdrawal on the bayc account

--- test/bayc.test.ts
+++ test/bayc.test.ts
@@ -15,11 +15,13 @@ import { parseEther } from "ethers";
 describe("Bored Ape Yacht Club [Test]", () => {
   let bayc: any;
   let user: HardhatEthersSigner;
+  let owner: HardhatEthersSigner;
 
   /** @description Prepares the contract and address prior to each test. */
   beforeEach(async () => {
     const signers = await hre.ethers.getSigners();
 
+    owner = signers[0];
     user = signers[1];
 
     // deploys a bored ape contract to test interactions with it
@@ -43,6 +45,22 @@ describe("Bored Ape Yacht Club [Test]", () => {
     it("properly sets the static URI", async () => {
       const uri = await bayc.tokenURI(0);
       expect(uri).to.equal("https://cdn.pr0xy.io/mofa/boredapetest.json");
+    });
+  });
+
+  /** @description Withdrawal functions for extracting the ETH. */
+  describe("Withdrawal", () => {
+    it("withdraws the ETH to the owner's account", async () => {
+      const startingBalance = await hre.ethers.provider.getBalance(owner);
+      await bayc.connect(user).mintApe(5, { value: parseEther("0.05") });
+      await bayc.connect(owner).withdraw();
+      const endingBalance = await hre.ethers.provider.getBalance(owner);
+
+      const delta = endingBalance - startingBalance;
+      const ether = Number(hre.ethers.formatUnits(delta.toString(), "ether"));
+
+      expect(ether).to.be.greaterThan(0.049);
+      expect(endingBalance).to.be.greaterThan(startingBalance);
     });
   });
 });
